fd7962c3b6fd643c9ef1a018e43e181a0e91c005
pasampat <priyasampat.college@gmail.com>
2025-08-05 21:22:50 -0400

    Remove .history folder from tracking

--- .history/main_20250805184159.py
+++
@@ -1,4 +0,0 @@
-from etl_utils import load_csv_to_sqlite
-
-# Process your first CSV into SQLite
-load_csv_to_sqlite("data/movies.csv")
--- .history/main_20250805191206.py
+++
@@ -1,3 +0,0 @@
-from etl_utils import load_csv_to_sqlite
-
-load_csv_to_sqlite("data/movies.csv", archive=False)
